4aa1c3a6652442e7afd967ed1b94cdb3658755a7
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-03-27 22:15:46 +0530

    learning React router

--- level-two/src/App.js
+++ level-two/src/App.js
@@ -1,24 +1,14 @@
 import "./App.css";
 import React from "react";
-import { Route, Routes } from "react-router-dom";
-import About from "./Components/About";
-import Dashboard from "./Components/Dashboard";
-import Home from "./Components/Home";
+
 
 function App() {
   return (
-    <React.Fragment>
-      <Routes>
-        <Route path="/" element={<Home />}></Route>
-      </Routes>
-      <Routes>
-        <Route path="/about" element={<About />}></Route>
-      </Routes>
-      <Routes>
-        <Route path="/dashboard" element={<Dashboard />}></Route>
-      </Routes>
-    </React.Fragment>
+   <React.Fragment>
+   <h1>Hello</h1>
+   </React.Fragment>
   );
 }
 
 export default App;
+ 
