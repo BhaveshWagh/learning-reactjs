c640090a9a6ea765c91f6e7f604d73dc0c746a21
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-04-14 17:38:42 +0530

    e commerce app using redux

---
+++ e-commerce-app/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import ProductListing from "./components/ProductListing";
+import ProductDetail from "./components/ProductDetail";
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Header />
+        <Routes>
+          <Route exact path="/" element={<ProductListing />} />
+          <Route path="/product/:productId" element={<ProductDetail />} />
+          <Route>404 Not Found</Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
