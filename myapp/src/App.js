eeb57a25dc48c3241dc24f8bd8ed662e04f1b5ac
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-05-04 21:43:58 +0530

    learned destructuring

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -2,14 +2,49 @@ import "./App.css";
 import Employee from "./components/Employee";
 
 function App() {
+  const employeeInfo = [
+    {
+      firstName: "Akshay",
+      lastName: "Winstin",
+      age: "23",
+      employeeId: "1",
+    },
+    {
+      firstName: "Sumit",
+      lastName: "pimple",
+      age: "30",
+      employeeId: "2",
+    },
+    {
+      firstName: "Vaibhav",
+      lastName: "Sundera",
+      age: "24",
+      employeeId: "3",
+    },
+    {
+      firstName: "Jasmin",
+      lastName: "Futera",
+      age: "25",
+      employeeId: "4",
+    },
+  ];
+
   return (
     <div className="App">
       <header className="App-header">
         <h1> Company Directory</h1>
-        <Employee firstName="Mayur"  lastName="Patil"/>
-        <Employee firstName="Dinesh" lastName="Ishi"/>
-        <Employee firstName="Moreshwar" lastName="Wagh"/>
-        <Employee firstName="Kapil" lastName="Wagh"/>
+        {employeeInfo.map((employee) => {
+          // const { firstName, lastName, age } = employee;
+          return (
+            <Employee key={employee.employeeId} {...employee} />
+            /*<Employee firstName={firstName} lastName={lastName} age={age} />*/
+          );
+        })}
+
+        {/*<Employee firstName="Mayur" lastName="Patil" age="14" />
+        <Employee firstName="Dinesh" lastName="Ishi" age="15" />
+        <Employee firstName="Moreshwar" lastName="Wagh" age="16" />
+        <Employee firstName="Kapil" lastName="Wagh" age="17" /> */}
       </header>
     </div>
   );
--- myapp/src/components/Employee.js
+++ myapp/src/components/Employee.js
@@ -2,12 +2,14 @@ import React from "react";
 
 const Employee = (props) => {
     // console.log(props)
+    const {firstName, lastName, age} = props;
   return (
     <React.Fragment>
-    <h5>Employee Name : {props.firstName} {props.lastName}</h5>
+  {/*<h5>Employee Name : {props.firstName} {props.lastName}</h5>*/}
+  <h5>Employee Name : {firstName} {lastName} Who's age is : {age}</h5>
     </React.Fragment>
     
   )
 };
-
+ 
 export default Employee;
