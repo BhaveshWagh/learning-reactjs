498a3e086786703e67ba3d14bd3e2040f8d1c85b
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-10-16 10:16:48 +0530

    eslint-disable

--- 06miniContext/src/context/UserContextProvider.jsx
+++ 06miniContext/src/context/UserContextProvider.jsx
@@ -2,17 +2,14 @@
 import React from "react";
 import UserContext from "./UserContext";
 
-const UserContextProvider = ({children}) => {
+const UserContextProvider = ({ children }) => {
+  const [user, setUser] = React.useState(null);
 
-    const [user,setUser] = React.useState(null)
-    
-    return(
-    
-    <UserContext.Provider value={{user, setUser}}>
-            {children}
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
     </UserContext.Provider>
-    
-    )
+  );
 };
 
 export default UserContextProvider;
