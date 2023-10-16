46640fd86b9f1574081750aada6ce31777fe9a39
BhaveshWagh <bhaveshbm2711@gmail.com>
2023-10-16 10:15:55 +0530

    es lint disable

--- 06miniContext/src/context/UserContextProvider.jsx
+++ 06miniContext/src/context/UserContextProvider.jsx
@@ -3,11 +3,15 @@ import React from "react";
 import UserContext from "./UserContext";
 
 const UserContextProvider = ({children}) => {
+
     const [user,setUser] = React.useState(null)
+    
     return(
-        <UserContext.Provider value={{user, setUser}}>
+    
+    <UserContext.Provider value={{user, setUser}}>
             {children}
-        </UserContext.Provider>
+    </UserContext.Provider>
+    
     )
 };
 
